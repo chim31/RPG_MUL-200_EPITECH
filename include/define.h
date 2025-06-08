8dc55e1562ebaad13640bc3238bb8c274c34d313
Roxane Koukoubou <roxane.koukoubou@epitech.eu>
2025-06-08 22:30:40 +0100

    adding the <include> directory containing the .h files

---
+++ include/define.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** MY_RPG
+** File description:
+** macro file.
+*/
+
+#ifndef DEFINE_H_
+    #include <SFML/Window/Export.h>
+    #include <SFML/Window/Types.h>
+    #include <SFML/System/Vector2.h>
+    #include <SFML/Graphics/Types.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/Window.h>
+    #include <SFML/System.h>
+    #include <SFML/Audio.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <stdbool.h>
+    #include <stdarg.h>
+    #include <stdint.h>
+    #include <stddef.h>
+    #define DEFINE_H_
+
+// si tu fais un define, mets ici, pareil pour les structures
+// l'autre .h c'est pour les fonctions
+
+#endif /* DEFINE_H_ */
---
+++ include/rpg.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** MY_PRG
+** File description:
+** macro file.
+*/
+
+#ifndef RPG_H_
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <dirent.h>
+    #include <crypt.h>
+    #include <termios.h>
+    #include <fcntl.h>
+    #include <errno.h>
+    #include <sys/wait.h>
+    #include <grp.h>
+    #include <pwd.h>
+    #include <time.h>
+    #include "define.h"
+    #define RPG_H_
+
+// pas de duplicat de include !!!
+// prototypes des fonctions uniquement !!!!!!!!!
+
+#endif /* RPG_H_ */
